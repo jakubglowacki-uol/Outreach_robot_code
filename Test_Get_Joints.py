c62dc06e7af828b6e629df59c881a2071512eb38
Jakub Glowacki <jakubg@liverpool.ac.uk>
2025-12-16 10:17:43 +0000

    adding testing

---
+++ Test_Get_Joints.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import time
+import argparse
+import math 
+# Add the directory containing robotiq_preamble.py to the Python search path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(current_dir, 'robotiq'))
+
+from utils.UR_Functions import URfunctions as URControl
+
+HOST = "192.168.10.2"
+PORT = 30003
+
+def main():
+    robot = URControl(ip="192.168.10.2", port=30003)
+    print(robot.get_current_joint_positions().tolist())
+    print(robot.get_current_tcp())
+if __name__ == '__main__':
+     main()
---
+++ test_rtde.py
@@ -0,0 +1,5 @@
+import rtde_control # For controlling the robot
+
+import rtde_receive # For receiving data from the robot
+
+import rtde_io # For robot IO
